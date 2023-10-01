f39186be751ae6d478e30ea86fc48acbfff72706
abghifareihand <info.abghifareihan@gmail.com>
2023-10-01 12:58:18 +0700

    update code

--- lib/data/services/wallet_service.dart
+++ lib/data/services/wallet_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:ewallet_app/common/constants.dart';
-import 'package:ewallet_app/data/models/user_edit_model.dart';
 import 'package:ewallet_app/data/services/local_service.dart';
 import 'package:http/http.dart' as http;
 
